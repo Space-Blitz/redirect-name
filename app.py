b957091b8ade96bc8264ec99ba3b3b59ca6939c9
patrickf949 <43245704+patrickf949@users.noreply.github.com>
2022-07-18 22:59:45 +0300

    enforce json responses

--- app.py
+++ app.py
@@ -7,40 +7,67 @@ app = Flask(__name__)
 
 
 def headers():
-    return {'Authorization':request.headers.get('Authorization')}
+    # return {'Authorization':request.headers.get('Authorization')}
+    return {}
     # return dict(request.headers)
 
 
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>',methods=['get'])
 def get_all(u_path):
-    url = api_url+u_path
-    return requests.get(url=url,headers=headers()).content
+    response=''
+    try:
+        url = api_url+u_path
+        response = requests.get(url=url,headers=headers())
+        return jsonify(response.json()),response.status_code
+    except:
+        return jsonify(response.json()),response.status_code
 
 
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>',methods=['post'])
 def post_all(u_path):
-    url = api_url+u_path
-    return requests.post(url=url,headers=headers(),json=request.get_json(force=True)).content
+    response=''
+    try:
+        url = api_url+u_path
+        response = requests.post(url=url,headers=headers(),json=request.get_json(force=True))
+        return jsonify(response.json()),response.status_code
+    except:
+        return jsonify(response.json()),response.status_code
 
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>',methods=['put'])
 def put_all(u_path):
-    url = api_url+u_path
-    return requests.post(url=url,headers=headers(),json=request.get_json(force=True)).content
+    response=''
+    try:
+        url = api_url+u_path
+        response = requests.put(url=url,headers=headers(),json=request.get_json(force=True))
+        return jsonify(response.json()),response.status_code
+    except:
+        return jsonify(response.json()),response.status_code
+
 
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>',methods=['patch'])
 def patch_all(u_path):
-    url = api_url+u_path
-    return requests.patch(url=url,headers=headers(),json=request.get_json(force=True)).content
+    response=''
+    try:
+        url = api_url+u_path
+        response = requests.patch(url=url,headers=headers(),json=request.get_json(force=True))
+        return jsonify(response.json()),response.status_code
+    except:
+        return jsonify(response.json()),response.status_code
 
 @app.route('/', defaults={'u_path': ''})
 @app.route('/<path:u_path>',methods=['delete'])
 def delete_all(u_path):
-    url = api_url+u_path
-    return requests.delete(url=url,headers=headers()).content
+    response=''
+    try:
+        url = api_url+u_path
+        response = requests.delete(url=url,headers=headers())
+        return jsonify(response.json()),response.status_code
+    except:
+        return jsonify(response.json()),response.status_code
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True)
